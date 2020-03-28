d78f3f9c65e30e390e1dc3236a5815e4145dddfd
kde6260 <kde6260@gmail.com>
2020-03-29 01:26:58 +0900

    remove newline to fail linting

--- app/app.py
+++ app/app.py
@@ -4,11 +4,9 @@ from flask import Flask
 
 flask_app = Flask(__name__, root_path=os.path.dirname(os.path.dirname(__file__)))
 
-
 @flask_app.route('/')
 def index():
     return 'Hello, This is DaEun Kim.'
-
 
 @flask_app.route('/info')
 def info():
